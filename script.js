6d31b6fc6a65b51ea49f83141505b7dddd2e8ce9
Shirahotsu <Shirahotsu@users.noreply.github.com>
2018-04-09 20:43:05 +0200

    diala

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 $( window ).scroll(function() {
     if($(window).scrollTop() > 400){
         $( "#header" ).addClass( "navbar-fixed" );
+        $( "#content" ).addClass( "contentA" );
+        $( "#content" ).removeClass( "contentB" );
         $( "#header" ).removeClass( "header" );
 
     }else{
         $( "#header" ).addClass( "header" );
         $( "#header" ).removeClass( "navbar-fixed" );
+        $( "#content" ).removeClass( "contentA" );
+        $( "#content" ).addClass( "contentB" );
+
 
 
     }
