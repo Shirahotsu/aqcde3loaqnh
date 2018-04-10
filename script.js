cd35b1e8d7108ab15b6916d07c3da039e73bf904
Shirahotsu <Shirahotsu@users.noreply.github.com>
2018-04-10 21:12:38 +0200

    content

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-$( window ).scroll(function() {
-    if($(window).scrollTop() > 400){
-        $( "#header" ).addClass( "navbar-fixed" );
-        $( "#content" ).addClass( "contentA" );
-        $( "#content" ).removeClass( "contentB" );
-        $( "#header" ).removeClass( "header" );
-
-    }else{
-        $( "#header" ).addClass( "header" );
-        $( "#header" ).removeClass( "navbar-fixed" );
-        $( "#content" ).removeClass( "contentA" );
-        $( "#content" ).addClass( "contentB" );
-
-
-
-    }
+$(document).ready(function(){
+    $(".button-collapse").sideNav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
 
 });
+// $( window ).scroll(function() {
+//     if($(window).scrollTop() > 400){
+//         $( "#header" ).addClass( "navbar-fixed" );
+//         $( "#content" ).addClass( "contentA" );
+//         $( "#content" ).removeClass( "contentB" );
+//         $( "#header" ).removeClass( "header" );
+//
+//     }else{
+//         $( "#header" ).addClass( "header" );
+//         $( "#header" ).removeClass( "navbar-fixed" );
+//         $( "#content" ).removeClass( "contentA" );
+//         $( "#content" ).addClass( "contentB" );
+//
+//
+//
+//     }
+//
+// });
 // $(document).ready(function(){
 //     $(".button-collapse").sideNav();
 //
 // });
 
-$(document).ready(function(){
-    $(".button-collapse").sideNav();
-});
