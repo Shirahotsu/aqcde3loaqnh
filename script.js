d5997a41683ffc77ecadc7827f4c81aba529e611
Shirahotsu <Shirahotsu@users.noreply.github.com>
2018-04-23 08:19:36 +0200

    0.9

--- script.js
+++ script.js
@@ -8,26 +8,15 @@ $(document).ready(function(){
 function toggleBlank(){
     $("#blank").removeClass("blank");
 }
-// $( window ).scroll(function() {
-//     if($(window).scrollTop() > 400){
-//         $( "#header" ).addClass( "navbar-fixed" );
-//         $( "#content" ).addClass( "contentA" );
-//         $( "#content" ).removeClass( "contentB" );
-//         $( "#header" ).removeClass( "header" );
-//
-//     }else{
-//         $( "#header" ).addClass( "header" );
-//         $( "#header" ).removeClass( "navbar-fixed" );
-//         $( "#content" ).removeClass( "contentA" );
-//         $( "#content" ).addClass( "contentB" );
-//
-//
-//
-//     }
-//
-// });
-// $(document).ready(function(){
-//     $(".button-collapse").sideNav();
-//
-// });
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        $("#banner").removeClass("container");
+    } else {
+       $("#banner").addClass("container");
+    }
+}
+
+var x = window.matchMedia("(max-width: 1300px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
 
